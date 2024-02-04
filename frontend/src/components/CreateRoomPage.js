36917a751d20d9c604ff1d66a203386e8e6aaac5
Merry Amonle <amonlemerry@gmail.com>
2024-02-04 20:53:23 +0100

    v3

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -10,6 +10,7 @@ import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
+
 export default class CreateRoomPage extends Component {
   defaultVotes = 2;
 
